5ff42cb30ad031c5c219543947ed02d95e4aa119
Ian Geckeler <ikcgeckeler@gmail.com>
2018-11-29 13:39:36 -0800

    add quotes

--- components/features/about.js
+++ components/features/about.js
@@ -3,25 +3,25 @@ React = require('react')
 const About = (props) => {
     return(
     <div>
-        Hey! My name’s Ian, I’m a junior software developer and data analyst. I thrive in startup environments and 
+        <p>{`Hey! My name's Ian, I’m a junior software developer and data analyst. I thrive in startup environments and 
         teams where people “wear many hats”. As an Applied and Computational Math major and a self-taught web developer, 
-        I am an entrepreneurial self-starter, an extremely quick learner, and a versatile team player. 
+        I am an entrepreneurial self-starter, an extremely quick learner, and a versatile team player.`} 
         <br></br><br></br>
-        I find that my talents and disposition work best in environments that are fast-paced and that value creative input.
+        {`I find that my talents and disposition work best in environments that are fast-paced and that value creative input.
         I love to construct plans and processes with high-autonomy and work on 
-        teams of positive, energetic, and talented individuals. 
+        teams of positive, energetic, and talented individuals.`} 
         <br></br><br></br>
-        I am currently interested in full-stack development, data science, and the application of both to improve the 
-        decisions of companies and people.
+        {`I am currently interested in full-stack development, data science, and the application of both to improve the 
+        decisions of companies and people.`}
         <br></br><br></br>
-        I'm bullish on the capability of technology to solve many of humanity's problems, but also wary of its misuse. 
+        {`I'm bullish on the capability of technology to solve many of humanity's problems, but also wary of its misuse. 
         For that reason, I want to be as involved as I can in crafting technologies which will increase the 
         autonomy of inidividuals around the globe. In the future, my sights are set on crafting an impactful software 
-        product and becoming involved in venture capital to support entrepreneurship and innovative technology.
+        product and becoming involved in venture capital to support entrepreneurship and innovative technology.`}
         <br></br><br></br>
-        In my free time, I read books on psychology, economics, philosophy, and business, and try to host as many discussions
-        about solving the world's problems as I can. I am outspoken about the limitations of our current political, educational, and healthcare systems, and hope to disrupt them in the future.
-    
+        {`In my free time, I read books on psychology, economics, philosophy, and business, and try to host as many discussions
+        about solving the world's problems as I can. I am outspoken about the limitations of our current political, educational, and healthcare systems, and hope to disrupt them in the future.`}
+        </p>
     </div>)
 }
 
